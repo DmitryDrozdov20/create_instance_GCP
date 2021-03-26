3d48ea2e051f9ae8769d46af64d49bd12882a152
DmitryDrozdov20 <dmitry.drozdov@hotmail.com>
2021-03-26 18:01:34 +0300

    vars

---
+++ vars.tf
@@ -0,0 +1,24 @@
+variable "node_count" {
+  default = "1"
+}
+
+variable "instance_tags" {
+  type    = list(string)
+  default = ["jenkins_clean", "prod"]
+}
+
+variable "machine_type" {
+  default = "e2-medium" 
+  # "e2-small"         // 2vCPU, 2GB RAM
+  # "e2-medium"        // 2vCPU, 4GB RAM
+  # "custom-4-4096"    // 4vCPU, 4GB RAM
+  # "custom-4-8192"    // 4vCPU, 8GB RAM
+}
+
+variable "region" {
+  default = "europe-north1"
+}
+
+variable "zone" {
+  default = "europe-north1-a"
+}
